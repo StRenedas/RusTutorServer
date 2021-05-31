0a6251a6d120be613d7f68d1b3b4669fc55b8117
Stanislav Sosedov <55929542+StRenedas@users.noreply.github.com>
2021-06-01 04:20:23 +0500

    db configured

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: 'localhost',
+    HOST: 'us-cdbr-east-04.cleardb.com',
     PORT: 3306,
-    DBUSER: 'root',
-    DBPASSWORD: 'root',
-    DBNAME: 'rustutor',
+    DBUSER: 'b6b4910116bef0',
+    DBPASSWORD: '603ee70c',
+    DBNAME: 'heroku_eaf7e8c4f2dbd2f',
     JWTSECRET: 'secret'
 }
