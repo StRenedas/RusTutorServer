e6daf0589676b1aeb2ea826b1ddeb534a5486614
Stanislav Sosedov <55929542+StRenedas@users.noreply.github.com>
2021-06-17 01:15:58 +0500

    deleted creds

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: 'us-cdbr-east-04.cleardb.com',
+    HOST: '',
     PORT: 3306,
-    DBUSER: 'b6b4910116bef0',
-    DBPASSWORD: '603ee70c',
-    DBNAME: 'heroku_eaf7e8c4f2dbd2f',
-    JWTSECRET: 'secret'
+    DBUSER: '',
+    DBPASSWORD: '',
+    DBNAME: '',
+    JWTSECRET: ''
 }
